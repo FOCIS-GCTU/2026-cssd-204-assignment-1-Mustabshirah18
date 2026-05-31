a18277f27ec07546b0a2fc1ff7c317b9e846ba2e
Mustabshirah18 <2425403331@live.gctu.edu.gh>
2026-05-31 11:06:29 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Mustabshirah Musah Abdullahi
+# STUDENT ID: 2425403331
+# Email: 242540331@live.gctu.edu.gh
+# Grader: Agustus Buckman
+#
+# On my honor, Mustabshirah Musah Abdullahi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print stylized initials MMA and their large-period blocks.
 
+    print()
+    print("...MMA")
+    print()
+
+    print("...MMMM....MMMM........MMMM....MMMM............AAAA.........")
+    print("...MMMM....MMMM........MMMM....MMMM............AAAA.........")
+    print("...MM.MM..MM.MM........MM.MM..MM.MM...........AA..AA........")
+    print("...MM.MM..MM.MM........MM.MM..MM.MM...........AA..AA........")
+    print("...MM.MM..MM.MM........MM.MM..MM.MM..........AAAAAAAA........")
+    print("...MM..MMMM..MM........MM..MMMM..MM..........AAAAAAAA........")
+    print("...MM..MMMM..MM........MM..MMMM..MM.........AA......AA.......")
+    print("...MM...MM...MM........MM...MM...MM.........AA......AA.......")
+    print("...MM...MM...MM...**...MM...MM...MM...**...AA........AA...**")
+    print("...MM...MM...MM...**...MM...MM...MM...**...AA........AA...**")
+
+    print()
 
 
 main()
